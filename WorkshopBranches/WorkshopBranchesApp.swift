a625f1e03fc9341c0bc8627d28a0485dec7260fc
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2025-06-24 14:17:37 -0300

    Initial Commit

---
+++ WorkshopBranches/WorkshopBranchesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkshopBranchesApp.swift
+//  WorkshopBranches
+//
+//  Created by Gustavo Munhoz Correa on 24/06/25.
+//
+
+import SwiftUI
+
+@main
+struct WorkshopBranchesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
